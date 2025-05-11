6d27cef8d3dd93972a6718e27f08728ef29a4cae
hilosov <kirilinka320@gmail.com>
2025-05-11 17:45:10 +0300

    3.7 travel.html

---
+++ scripts/travel.js
@@ -0,0 +1,25 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded', () => {
+const slider = document.querySelector('.swiper');
+
+    if (slider) {
+        const swiper = new Swiper(slider, {
+            // Дополнительные параметры
+            slidesPerView: 1, // Количество слайдов на экране
+            spaceBetween: 30, // Расстояние между слайдами
+            loop: true,  // Зацикливание слайдов
+
+            // Пагинация
+            pagination: {
+                el: '.swiper-pagination',
+            },
+
+            // Навигационные стрелки
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+    }
+});
